7d7ad1100fe03167352724d8fb5a9ef210286db2
r4khic <arinovrakhic@gmail.com>
2020-08-16 23:12:27 +0600

    data checker was modified

--- data_checker.py
+++ data_checker.py
@@ -1,4 +1,5 @@
 import pymysql
+import csv
 
 
 def get_connect():
@@ -14,12 +15,12 @@ def get_connect():
 
 def calculate_data_for_colors(connection):
     colors_id = []
-    query = 'SELECT id FROM `wheel_results` ORDER BY `wheel_results`.`game_run_time` ASC'
+    query = 'SELECT correct_id FROM `wheel_results` LIMIT 10'
     connection.execute(query)
     results = connection.fetchall()
     for result in results:
-        plus_result_id = result[0] + 9
-        plus_result_id1 = result[0] + 10
+        plus_result_id = result[0] + 8
+        plus_result_id1 = result[0] + 9
         colors_id.append(plus_result_id)
         colors_id.append(plus_result_id1)
     return colors_id
@@ -27,25 +28,28 @@ def calculate_data_for_colors(connection):
 
 def get_data(connection, colors_id):
     data = []
-    query = 'SELECT * FROM `wheel_results` WHERE id = %s'
+    query = 'SELECT * FROM `wheel_results` WHERE id = %s LIMIT 10'
     for color_id in colors_id:
-        print(color_id)
-        # connection.execute(query, color_id)
-        # result = connection.fetchall()
-        # data.append(result)
+        connection.execute(query, color_id)
+        result = connection.fetchall()
+        print(result)
+        data.append(result)
     return data
 
 
 def csv_write(data):
-    pass
-
+    with open('wheel_data.csv', 'w') as f:
+        writer = csv.writer(f)
+        for row in data:
+            writer.writerow(row)
+    print('all is ok')
 
 
 def main():
     connection = get_connect()
     colors_id = calculate_data_for_colors(connection)
     data = get_data(connection, colors_id)
-    # csv_write(data)
+    csv_write(data)
 
 
 if __name__ == '__main__':
